10df7b8b947871dd10a419ae329c33d26d14e05f
Alejandro0243563 <0243563@up.edu.mx>
2025-07-24 20:39:16 -0600

    Update Success.tsx

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -27,7 +27,7 @@ const Success = () => {
               <img 
                 src="/lovable-uploads/baa877cc-6d08-4d7e-ba07-2f4a014b2a59.png" 
                 alt="CARAVEL Logo" 
-                className="h-8 w-auto"
+                className="h-16 w-auto"
               />
             </div>
           </div>
